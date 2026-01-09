319d8fa1ca389d2e3e4de103a0583f76f0645243
ferrari-nico-edu-acc <ferrari.nico@iisgalvanimi.edu.it>
2026-01-09 09:46:32 +0000

    add missing stuff to es7

--- es7/script.js
+++ es7/script.js
@@ -4,8 +4,15 @@ const prices = document.querySelector("#prices");
 const calculate = document.querySelector("#calculate");
 /** @type {HTMLHeadingElement} */
 const result = document.querySelector("#result");
+/** @type {HTMLHeadingElement} */
+const budget_alert = document.querySelector("#budget_alert");
 
 calculate.addEventListener("click",() => {
     const total = prices.value.replaceAll(/\s/g,"").split(",").map(val => Number(val)).filter(val => isFinite(val)).reduce((acc,cur) => acc + cur,0);
-    result.innerText = `Totale: ${total}`;
+    result.innerText = `Totale: ${total}€`;
+    if (total > 100) {
+        budget_alert.style.display = "block";
+    } else {
+        budget_alert.style.removeProperty("display");
+    }
 })
